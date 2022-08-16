b2883945bfd21fec1f65602a4ad3dab1494a8eb1
Axel Vasquez <a.axelvc@gmail.com>
2022-08-15 23:54:15 -0500

    add remove_template_string option

--- lua/template-string/init.lua
+++ lua/template-string/init.lua
@@ -7,6 +7,7 @@ local M = {}
 M.options = {
 	filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
 	jsx_brackets = true,
+	remove_template_string = false,
 }
 
 M.group = augroup("TemplateString", { clear = true })
@@ -23,16 +24,12 @@ function M.get_string_node()
 		return node
 	elseif type == "string_fragment" then
 		return node:parent()
+	elseif type == "template_string" then
+		return node
 	end
 end
 
-function M.convert_to_template_string()
-	local buf = vim.api.nvim_win_get_buf(0)
-	local node = M.get_string_node()
-
-	-- stylua: ignore
-	if not node then return end
-
+function M.handle_string(node, buf)
 	local text = vim.treesitter.get_node_text(node, buf)
 
 	-- stylua: ignore
@@ -55,6 +52,47 @@ function M.convert_to_template_string()
 	vim.api.nvim_buf_set_text(buf, sr, sc, er, ec, { new_text })
 end
 
+function M.handle_template(node, buf)
+	local text = vim.treesitter.get_node_text(node, buf)
+
+	if text:match("${.*}") or text:match("\n") or node:parent():type() == "call_expression" then
+		return
+	end
+
+	-- change backticks to quotes
+	-- TODO: detect which quotes are used in the file
+	local new_text = "'" .. text:sub(2, -2) .. "'"
+
+	if node:parent():type() == "jsx_expression" then
+		local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+		-- replace node with parent to remove jsx brackets
+		node = node:parent()
+
+		-- moving the cursor to stay in the same position after adding brackets
+		vim.api.nvim_win_set_cursor(0, { row, col - 1 })
+	end
+
+	-- replace text with template string
+	local sr, sc, er, ec = node:range()
+	vim.api.nvim_buf_set_text(buf, sr, sc, er, ec, { new_text })
+end
+
+function M.handle_type()
+	local node = M.get_string_node()
+
+	-- stylua: ignore
+	if not node then return end
+
+	local buf = vim.api.nvim_win_get_buf(0)
+
+	if node:type() == "string" then
+		M.handle_string(node, buf)
+	elseif M.options.remove_template_string then
+		M.handle_template(node, buf)
+	end
+end
+
 function M.setup(options)
 	M.options = vim.tbl_extend("force", M.options, options or {})
 
@@ -66,10 +104,10 @@ function M.setup(options)
 				return
 			end
 
-			autocmd("CursorHoldI", {
+			autocmd("TextChangedI", {
 				group = M.group,
 				buffer = ev.buf,
-				callback = M.convert_to_template_string,
+				callback = M.handle_type,
 			})
 		end,
 	})
